436f5cb8967788991a9ebf5ad7680e67e696e21a
CodingHammy <Will.hamilton.job@gmail.com>
2025-05-20 13:57:47 +0100

    added 1.5sec delay and navigation to next route on successful credentals

--- Frontend/src/components/AuthForm.tsx
+++ Frontend/src/components/AuthForm.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 type AuthFormProps = {
@@ -8,6 +9,17 @@ export default function AuthForm({ type }: AuthFormProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSuccess = (location: string) => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      navigate(location, { replace: true });
+    }, 1500);
+  };
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -30,8 +42,10 @@ export default function AuthForm({ type }: AuthFormProps) {
         if (type === 'login') {
           localStorage.setItem('token', data.token);
           setMessage('Login successful!');
+          handleSuccess('/dashboard');
         } else {
           setMessage('Registration successful! You can now log in.');
+          handleSuccess('/');
         }
         setEmail('');
         setPassword('');
@@ -48,31 +62,35 @@ export default function AuthForm({ type }: AuthFormProps) {
       <h2 className='text-2xl font-bold mb-4'>
         {type === 'register' ? 'Register' : 'Login'}
       </h2>
-      <form onSubmit={handleRegister} className='space-y-4'>
-        <input
-          type='email'
-          placeholder='email'
-          className='w-full p-2 border rounded'
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type='password'
-          placeholder='password'
-          className='w-full p-2 border rounded'
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-        <button
-          type='submit'
-          className='w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700'
-        >
-          {type === 'register' ? 'Register' : 'Login'}
-        </button>
-        {message && <p className='mt-4 text-center'>{message}</p>}
-      </form>
+      {!isLoading ? (
+        <form onSubmit={handleRegister} className='space-y-4'>
+          <input
+            type='email'
+            placeholder='email'
+            className='w-full p-2 border rounded'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type='password'
+            placeholder='password'
+            className='w-full p-2 border rounded'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+          <button
+            type='submit'
+            className='w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700'
+          >
+            {type === 'register' ? 'Register' : 'Login'}
+          </button>
+        </form>
+      ) : (
+        <p className='mx-auto text-2xl font-bold'>Loading ...</p>
+      )}
+      {message && <p className='mt-4 text-center'>{message}</p>}
     </div>
   );
 }
