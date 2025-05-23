c4e1f78b6104d2799d589a10021c8712e6ec6377
CodingHammy <Will.hamilton.job@gmail.com>
2025-05-23 12:37:24 +0100

    added validate token function to comunicate with the backend and see if tokens have exspired

--- Frontend/src/context/AuthContext.tsx
+++ Frontend/src/context/AuthContext.tsx
@@ -2,15 +2,17 @@ import { createContext, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 type AuthContextType = {
-  isloggedIn: boolean;
+  isAuthenticated: boolean;
   token: string | null;
   login: (token: string) => void;
   logout: () => void;
+  validateToken: () => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthenticated, setAuthenticated] = useState(false);
   const [token, setToken] = useState<string | null>(() => {
     return localStorage.getItem('token');
   });
@@ -28,10 +30,34 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     navigate('/', { replace: true });
   };
 
-  const isloggedIn = !!token;
+  const validateToken = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) return;
+
+    try {
+      const res = await fetch('http://localhost:5000/api/users/dashboard', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (res.ok) {
+        setAuthenticated(true);
+      } else {
+        logout();
+      }
+    } catch (error) {
+      console.error('Error validating token:', error);
+      logout();
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ isloggedIn, token, login, logout }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, token, login, logout, validateToken }}
+    >
       {children}
     </AuthContext.Provider>
   );
