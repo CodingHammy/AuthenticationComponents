1fee8df4a6e85782c3c0d990ae3302dbfd23486e
CodingHammy <Will.hamilton.job@gmail.com>
2025-05-24 17:26:17 +0100

    added navbar with logout function and login and register links

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,22 +3,26 @@ import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import RegisterPage from './pages/RegisterPage';
 import PrivateRoute from './components/PrivateRoute';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
 
-      <Route
-        path='/dashboard'
-        element={
-          <PrivateRoute>
-            <DashboardPage />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <DashboardPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
